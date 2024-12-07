2f16458b31b6164be625eb98efbe026b77f3e892
luongduy0906 <luongducduy0906@gmail.com>
2024-12-07 17:09:14 +0700

    Them chuc nang tìm category voi id

--- tlunews/models/Category.php
+++ tlunews/models/Category.php
@@ -6,5 +6,12 @@ class Category {
         $stmt = $db->query("SELECT * FROM categories");
         return $stmt->fetchAll();
     }
+
+    public static function getCategoriesById($id) {
+        $db = connDB::getConnection();
+        $stmt = $db->prepare("SELECT * FROM categories WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
 }
 ?>
