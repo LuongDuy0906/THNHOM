e641aa2305b361c97264ca68b1b30f25c04bc542
luongduy0906 <luongducduy0906@gmail.com>
2024-12-07 19:35:12 +0700

    hoan thien category

--- tlunews/models/Category.php
+++ tlunews/models/Category.php
@@ -13,5 +13,23 @@ class Category {
         $stmt->execute([$id]);
         return $stmt->fetch();
     }
+
+    public static function add($name) {
+        $db = connDB::getConnection();
+        $stmt = $db->prepare("INSERT INTO categories (name) VALUES (?)");
+        return $stmt->execute([$name]);
+    }
+
+    public static function update($id, $name) {
+        $db = connDB::getConnection();
+        $stmt = $db->prepare("UPDATE categories SET name = ? WHERE id = ?");
+        return $stmt->execute([$name, $id]);
+    }
+
+    public static function delete($id) {
+        $db = connDB::getConnection();
+        $stmt = $db->prepare("DELETE FROM categories WHERE id = ?");
+        return $stmt->execute([$id]);
+    }
 }
 ?>
